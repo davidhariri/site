0d69f6bf07e606aa0fee082154d4d2c7c1a8f0ad
David Hariri <david.hariri@gmail.com>
2023-10-29 01:28:21 -0400

    Removes Posthog, adds Pirsch

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import sentry_sdk
 
 from service.page import get_all_page_paths_and_pages, get_all_pages_sorted
 from service.post import ALL_POSTS, get_all_posts
-from service.posthog import track_request
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
@@ -29,7 +28,6 @@ def render_not_found(_):
 
 
 @app.get("/")
-@track_request
 @cache.cached(timeout=60)
 def render_home():
     latest_posts = get_all_posts()[:3]
@@ -39,7 +37,6 @@ def render_home():
 
 
 @app.get("/blog/")
-@track_request
 @cache.cached(timeout=60)
 def render_blog_index():
     return render_template(
@@ -48,7 +45,6 @@ def render_blog_index():
 
 
 @app.get("/blog/<string:post_path>/")
-@track_request
 @cache.memoize(timeout=3600)
 def render_blog_post(post_path: str):
     try:
@@ -61,7 +57,6 @@ def render_blog_post(post_path: str):
 
 @app.get("/feed/")
 @app.get("/rss/")
-@track_request
 @cache.cached(timeout=60)
 def read_feed():
     items = [
@@ -85,7 +80,6 @@ def read_feed():
 
 
 @app.get("/<string:page_path>/")
-@track_request
 @cache.memoize(timeout=3600)
 def render_page(page_path: str):
     try:

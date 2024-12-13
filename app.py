68047aba28b83323c342f3ec8688ee8c030dd273
David Hariri <david.hariri@gmail.com>
2024-12-13 16:48:33 +1100

    Adds BlueSky integration

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ from rfeed import Item as RSSItem, Feed as RSSFeed  # type: ignore
 import sentry_sdk
 from slugify import slugify
 import tweepy
+from atproto import Client as AtProtoSyncClient
 
 from config import settings
 from service.page import get_all_page_paths_and_pages, get_all_pages_sorted
-from service.post import create_post, get_all_tags, get_posts, get_posts_index, Post
+from service.post import create_post, get_all_tags, get_posts, get_posts_index
 
 sentry_sdk.init(
     dsn=settings.SENTRY_DSN,
@@ -26,14 +27,14 @@ app.jinja_env.auto_reload = settings.DEBUG
 
 cache.init_app(app)
 
+
 @app.context_processor
 def inject_globals():
     return {
-        'og_title': settings.OG_TITLE,
-        'og_description': settings.OG_DESCRIPTION, 
-        'og_url': settings.OG_URL
+        "og_title": settings.OG_TITLE,
+        "og_description": settings.OG_DESCRIPTION,
+        "og_url": settings.OG_URL,
     }
-
 
 
 @app.errorhandler(404)
@@ -56,12 +57,17 @@ def render_blog_index():
     tag = request.args.get("tagged")
     posts = get_posts()
     all_tags = get_all_tags()
-    
+
     if tag:
         posts = [post for post in posts if post.tags is not None and tag in post.tags]
-    
+
     return render_template(
-        "blog.html", title="Blog", posts=posts, tags=all_tags, tagged=tag, pages=get_all_pages_sorted()
+        "blog.html",
+        title="Blog",
+        posts=posts,
+        tags=all_tags,
+        tagged=tag,
+        pages=get_all_pages_sorted(),
     )
 
 
@@ -89,7 +95,7 @@ def read_feed():
         )
         for post in get_posts()
     ]
-    feed = RSSFeed( # TODO: Read this from a YAML config file
+    feed = RSSFeed(  # TODO: Read this from a YAML config file
         title="David Hariri",
         link=f"{settings.FQD}/blog/",
         description="The blog of David Hariri. Programming, design, and more.",
@@ -110,36 +116,44 @@ def render_page(page_path: str):
 
     return render_template("page.html", page=page, pages=get_all_pages_sorted())
 
+
 # Micropub Logic
 
+
 def verify_access_token() -> bool:
-    auth_header = request.headers.get('Authorization', '')
-    if not auth_header.startswith('Bearer '):
+    """
+    Verify the access token in the Authorization header.
+    """
+    auth_header = request.headers.get("Authorization", "")
+    if not auth_header.startswith("Bearer "):
         return False
-    token = auth_header.split(' ')[1]
+    token = auth_header.split(" ")[1]
     return token == settings.MICROPUB_SECRET
 
+
 def upload_file_to_s3(file, bucket_name, file_key):
+    """
+    Upload a file to an S3 bucket.
+    """
     s3 = boto3.client(
         "s3",
         aws_access_key_id=settings.S3_ACCESS_KEY,
-        aws_secret_access_key=settings.S3_SECRET_KEY
+        aws_secret_access_key=settings.S3_SECRET_KEY,
     )
     try:
         s3.upload_fileobj(
-            file,
-            bucket_name,
-            file_key,
-            ExtraArgs={
-                "ContentType": file.content_type
-            }
+            file, bucket_name, file_key, ExtraArgs={"ContentType": file.content_type}
         )
     except NoCredentialsError:
         abort(500, description="AWS credentials not available.")
     except ClientError as e:
         abort(500, description=f"Client error: {e}")
 
+
 def post_tweet(post_content: str):
+    """
+    Post a tweet to Twitter.
+    """
     client = tweepy.Client(
         bearer_token=settings.TWITTER_API_BEARER_TOKEN,
         consumer_key=settings.TWITTER_API_CONSUMER_KEY,
@@ -149,9 +163,19 @@ def post_tweet(post_content: str):
     )
     client.create_tweet(text=post_content)
 
-@app.route('/micropub', methods=['GET', 'POST'])
+
+def post_bsky(post_content: str):
+    """
+    Post a blog post to bluesky
+    """
+    client = AtProtoSyncClient()
+    client.login(settings.BSKY_HANDLE, settings.BSKY_PASSWORD)
+    client.send_post(text=post_content)
+
+
+@app.route("/micropub", methods=["GET", "POST"])
 def micropub():
-    if request.method == 'GET':
+    if request.method == "GET":
         # Provide metadata about your Micropub endpoint
         response = {
             "actions": ["create", "update", "delete"],
@@ -159,34 +183,38 @@ def micropub():
             "syndicate-to": [
                 f"{settings.FQD}/blog/",
             ],
-            "media-endpoint": f"{settings.FQD}/micropub/media/"
+            "media-endpoint": f"{settings.FQD}/micropub/media/",
         }
         return jsonify(response)
-    elif request.method == 'POST':
+    elif request.method == "POST":
         if not verify_access_token():
             abort(401, description="Invalid or missing access token.")
-    
+
         data = request.get_json()
         if not data:
             abort(400, description="Invalid JSON payload.")
-        
-        h = data.get('type', ['entry'])[0]  # default to 'entry'
-        if h != 'h-entry':
+
+        h = data.get("type", ["entry"])[0]  # default to 'entry'
+        if h != "h-entry":
             abort(400, description="Unsupported type.")
 
-        properties = data.get('properties', {})
-        content_list = properties.get('content', [''])
+        properties = data.get("properties", {})
+        content_list = properties.get("content", [""])
 
-        if not isinstance(content_list, list) or not content_list or not isinstance(content_list[0], str):
+        if (
+            not isinstance(content_list, list)
+            or not content_list
+            or not isinstance(content_list[0], str)
+        ):
             abort(400, description="Invalid content format.")
-        
+
         content = content_list[0].strip()
-        
+
         if not content:
             abort(400, description="Missing content.")
 
-        title = properties.get('name', [None])[0]
-        categories = properties.get('category', [])  # tags
+        title = properties.get("name", [None])[0]
+        categories = properties.get("category", [])  # tags
 
         url_slug = slugify(title) if title else str(uuid.uuid4())
 
@@ -195,59 +223,69 @@ def micropub():
             content=content,
             url_slug=url_slug,
             tags=set(categories) if categories else None,
-            description=properties.get('summary', [None])[0]
+            description=properties.get("summary", [None])[0],
         )
         post = result.post
         updated_existing = result.updated_existing
 
         post_url = urljoin(settings.FQD, f"/blog/{post.url_slug}/")
+        post_social_content = f"{post.title}\n\n{post.description}\n\n🔗 {post_url}"
 
+        # Post to Twitter if configured in the environment
         if settings.TWITTER_API_BEARER_TOKEN and not updated_existing:
             # NOTE: You need more than just a bearer token to use the Twitter API to post to your own account
             #       See post_tweet()
             try:
-                post_tweet(f"{post.description}\n\n🔗 {post_url}")
+                post_tweet(post_social_content)
             except Exception as e:
                 sentry_sdk.capture_exception(e)
-        
+
+        # Post to bluesky if configured in the environment
+        if settings.BSKY_HANDLE and settings.BSKY_PASSWORD and not updated_existing:
+            try:
+                post_bsky(post_social_content)
+            except Exception as e:
+                sentry_sdk.capture_exception(e)
+
         # Clear the cache for the affected pages
         cache.clear()
 
         response = jsonify({"url": post_url})
         response.status_code = 201
-        response.headers['Location'] = post_url
+        response.headers["Location"] = post_url
         return response
 
-@app.route('/micropub/media/', methods=['POST'])
-def micropub_media():    
+
+@app.route("/micropub/media/", methods=["POST"])
+def micropub_media():
     if not verify_access_token():
         abort(401, description="Invalid or missing access token.")
-    
-    if 'file' not in request.files:
+
+    if "file" not in request.files:
         abort(400, description="No file part in the request.")
-    
-    file = request.files['file']
-    if file.filename == '':
+
+    file = request.files["file"]
+    if file.filename == "":
         abort(400, description="No selected file.")
-    
+
     # Check the file size using the Content-Length header
-    content_length = request.headers.get('Content-Length', type=int)
+    content_length = request.headers.get("Content-Length", type=int)
     if content_length is not None and content_length > 10 * 1024 * 1024:  # 10 MB
         abort(400, description="File is too large.")
-    
-    allowed_extensions = {'gif', 'jpg', 'jpeg', 'png', 'webp', 'mp4'}
-    file_ext = file.filename.rsplit('.', 1)[-1].lower()
+
+    allowed_extensions = {"gif", "jpg", "jpeg", "png", "webp", "mp4"}
+    file_ext = file.filename.rsplit(".", 1)[-1].lower()
     if file_ext not in allowed_extensions:
         abort(400, description="Unsupported file type.")
-    
+
     # Generate a unique filename
     unique_filename = f"{uuid.uuid4()}.{file_ext}"
-    
+
     # Upload the file to S3
     upload_file_to_s3(file, settings.S3_BUCKET_NAME, unique_filename)
-    
+
     file_url = f"https://{settings.S3_BUCKET_NAME}.s3.amazonaws.com/{unique_filename}"
     response = jsonify({"url": file_url})
     response.status_code = 201
-    response.headers['Location'] = file_url
+    response.headers["Location"] = file_url
     return response
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 
 load_dotenv()
 
+
 class Settings(BaseSettings):
     MONGODB_URI: str = os.environ["MONGODB_URI"]
     DATABASE_NAME: str = os.environ["DATABASE_NAME"]
@@ -17,11 +18,16 @@ class Settings(BaseSettings):
     TWITTER_API_CONSUMER_KEY: str | None = os.getenv("TWITTER_API_CONSUMER_KEY")
     TWITTER_API_CONSUMER_SECRET: str | None = os.getenv("TWITTER_API_CONSUMER_SECRET")
     TWITTER_API_ACCESS_TOKEN: str | None = os.getenv("TWITTER_API_ACCESS_TOKEN")
-    TWITTER_API_ACCESS_TOKEN_SECRET: str | None = os.getenv("TWITTER_API_ACCESS_TOKEN_SECRET")
+    TWITTER_API_ACCESS_TOKEN_SECRET: str | None = os.getenv(
+        "TWITTER_API_ACCESS_TOKEN_SECRET"
+    )
     TWITTER_API_BEARER_TOKEN: str | None = os.getenv("TWITTER_API_BEARER_TOKEN")
     DEBUG: bool = os.getenv("FLASK_DEBUG") == "1"
     OG_TITLE: str = "David Hariri"
     OG_URL: str = "https://dhariri.com"
     OG_DESCRIPTION: str = "David's place on the web"
+    BSKY_HANDLE: str | None = os.getenv("BSKY_HANDLE")
+    BSKY_PASSWORD: str | None = os.getenv("BSKY_PASSWORD")
+
 
 settings = Settings()
